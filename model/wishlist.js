834ea4b40110a96fbb65b000eee8ac62b4e41273
VanWrath <31023745+VanWrath@users.noreply.github.com>
2019-03-06 22:39:52 -0500

    Initial commit

---
+++ model/wishlist.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; //creates the scheme behind your data
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var wishList = new Schema({
+    title: {type: String, default: "Your Wish List"},   //more options for types.
+    products: [{type: ObjectId, ref: 'Product'}]       //collection of products. reference type is products that was exported.
+});
+
+module.exports = mongoose.model('WishList', wishList);
