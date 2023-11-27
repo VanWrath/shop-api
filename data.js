fc4d6e79184ea69e45de16d500c76c3cfe4e6cfa
Kyle <31023745+VanWrath@users.noreply.github.com>
2023-11-26 23:08:39 -0500

    Updated mongodb commands

---
+++ data.js
@@ -0,0 +1,13 @@
+db.products.insertOne({"price":15.99,"title":"Vault Boy Bobble Head","imgUrl":"https://s3-us-west-2.amazonaws.com/devslopesvideo/vault.JPG"})
+db.products.insertOne({"price":129.50,"title":"Link's Shield","imgUrl":"https://s3-us-west-2.amazonaws.com/devslopesvideo/shield.png"})
+db.products.insertOne({"price":399.95,"title":"Lancer","imgUrl":"https://s3-us-west-2.amazonaws.com/devslopesvideo/lancer.jpg"})
+db.products.insertOne ({"price":199.99,"title":"Genji Sword","imgUrl":"https://gear.blizzard.com/media/catalog/product/cache//1024x/e9c3970ab036de70892d86c6d221abfe/o/w/ow-genji-sword-group-gallery.png"})
+db.products.insertOne({"price":108.99,"title":"MG Gundam Unicorn building kit (1/100 scale)","imgUrl":"https://images-na.ssl-images-amazon.com/images/I/71XxYPUNuBL._SX425_.jpg"})
+db.products.insertOne({"price":14.99,"title":"Pikachu Plush","imgUrl":"https://images-na.ssl-images-amazon.com/images/I/71WeXKeyBpL._SX425_.jpg"})
+db.products.insertOne({"price":79.99,"title":"Keyblade Replica","imgUrl":"https://images-na.ssl-images-amazon.com/images/I/71Asij72cwL._SX425_.jpg"})
+db.products.insertOne({"price":19.99,"title":"Fortnite Hoodie","imgUrl":"https://cdn.shopify.com/s/files/1/2620/5964/products/fortnite-classic-logo-hoodie-unisex-white-small-fornite-sweatshirts-customcat-hatvat_380_900x.jpg?v=1534228337"})
+db.products.insertOne({"price":9.99,"title":"Superman Funko Pop Figure","imgUrl":"https://images-na.ssl-images-amazon.com/images/I/51QNB7LghaL._SX425_.jpg"})
+db.products.insertOne({"price":99.99,"title":"Thanos Infinity Gauntlet","imgUrl":"https://lumiere-a.akamaihd.net/v1/images/file_2baaba20.jpeg?width=1200&region=0%2C0%2C2000%2C2000&quality=8"})
+
+
+db.products.insertOne({"price":0.00,"title":"","imgUrl":""})
